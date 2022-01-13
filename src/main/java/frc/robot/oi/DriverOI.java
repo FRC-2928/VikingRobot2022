4f20d452c784cc8e7475f0343ef48fe8f544a831
BenFryHolman <himbafry@gmail.com>
2022-01-12 19:02:00 -0800

    added oi

---
+++ src/main/java/frc/robot/oi/DriverOI.java
@@ -0,0 +1,89 @@
+package frc.robot.oi;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.oi.DriverOI;
+
+public class DriverOI {
+    private XboxController m_controller;
+
+    public DriverOI(XboxController controller) {
+        m_controller = controller;
+    }
+
+    // ---------------- Intake ----------------------------
+
+
+    // public Button getGroundIntakeButton() {
+    //     return new Button(() -> m_controller.getLeftTriggerAxis() > 0.2);
+    // }
+
+
+    // public Button getStationIntakeButton() {
+    //     return new JoystickButton(m_controller, XboxController.Button.kLeftBumper.value);
+    // }
+
+    // ---------------- Climber ----------------------------
+
+
+    // public Button getClimbTrigger() {
+    //     return new JoystickButton(m_controller, XboxController.Axis.kRightTrigger.value);
+    // }
+
+    // ---------------- Shooting ----------------------------
+
+
+    // public Button getAutoShootingButton() {
+    //     return new JoystickButton(m_controller, XboxController.Button.kRightBumper.value);
+    // }
+
+
+    // public Button getSetpointShootingButton(){
+    //     return new Button(() -> m_controller.getRightTriggerAxis() > 0.2);
+    // }
+
+
+    // public Button getFenderShotButton() {
+    //     return new Button(() -> m_controller.getPOV() == 180);
+    // }
+
+
+    // public Button getInitiationlineShotButton() {
+    //     return new Button(() -> m_controller.getPOV() == 0);
+    // }
+
+
+    // public Button getShooterDebugButton() {
+    //     return new Button(() -> m_controller.getPOV() == 90);
+    // }
+
+
+    // public Button getFeedButton() {
+    //     return new Button(() -> m_controller.getRightTriggerAxis() > 0.2);
+    // }
+
+    // ---------------- Drivetrain ----------------------------
+
+
+    public Button getShiftLowButton() {
+        return new JoystickButton(m_controller, XboxController.Button.kX.value);
+    }
+
+
+    public Button getShiftHighButton() {
+        return new JoystickButton(m_controller, XboxController.Button.kY.value);
+    }
+
+
+    public DoubleSupplier getMoveSupplier() {
+        return () -> -m_controller.getLeftY();
+    }
+
+
+    public DoubleSupplier getRotateSupplier() {
+        return () -> m_controller.getRightX();
+    }
+}
