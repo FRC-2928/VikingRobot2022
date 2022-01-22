ad9fc7269d9b2cc65eb049c1779146191dcf5f08
BenFryHolman <himbafry@gmail.com>
2022-01-21 20:03:13 -0800

    changes of pid gains

--- src/main/java/frc/robot/commands/DriveDistanceProfiled.java
+++ src/main/java/frc/robot/commands/DriveDistanceProfiled.java
@@ -23,7 +23,7 @@ public class DriveDistanceProfiled extends ProfiledPIDCommand {
         // The ProfiledPIDController used by the command
         new ProfiledPIDController(
             // The PID gains
-            32,
+            5,
             0,
             0,
             // The motion profile constraints
