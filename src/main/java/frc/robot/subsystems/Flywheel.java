55ac324bf03d29fd376858242c5c642e03990362
Jane McKelvey <janemckelvey4@gmail.com>
2022-01-29 15:23:48 -0800

    added some subsystems

--- src/main/java/frc/robot/subsystems/Flywheel.java
+++ src/main/java/frc/robot/subsystems/Flywheel.java
@@ -6,17 +6,12 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+public class Flywheel extends SubsystemBase {
+  /** Creates a new Flywheel. */
+  public Flywheel() {}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
   }
 }
