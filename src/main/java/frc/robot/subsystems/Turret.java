ce9bd0835e85e0a95d7e8759e469b6ec128fef2e
Jane McKelvey <janemckelvey4@gmail.com>
2022-01-30 13:28:53 -0800

    instantiate limelight in turret

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -5,10 +5,24 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Limelight.Limelights;
+import frc.robot.subsystems.Limelight;
+import frc.robot.Constants;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class Turret extends SubsystemBase {
   /** Creates a new Turret. */
-  public Turret() {}
+
+  private final Limelight m_turretLimelight;
+  private final TalonSRX m_turretMotor;
+
+  
+  public Turret() {
+    m_turretLimelight = new Limelight(Limelights.TURRET);
+    m_turretMotor = new TalonSRX(Constants.RobotMap.kTurretSparkMax);
+  }
+
 
   @Override
   public void periodic() {
