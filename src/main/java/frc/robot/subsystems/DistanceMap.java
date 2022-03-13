24541043a7d1eead3601199c7987b7aa0a7469cd
BenFryHolman <himbafry@gmail.com>
2022-03-12 16:09:07 -0800

    distanceMap changes

--- src/main/java/frc/robot/subsystems/DistanceMap.java
+++ src/main/java/frc/robot/subsystems/DistanceMap.java
@@ -19,14 +19,23 @@ public class DistanceMap {
         return m_instance;
     }
 
-    private final Map<Double, Integer> m_tickspersecond = new HashMap<>();
+    private final Map<Integer, Integer> m_tickspersecond = new HashMap<>();
 
     public void loadMaps() {
-       
-        m_tickspersecond.put(5.5, 3500);
-     
-        m_tickspersecond.put(10.0, 10000); // INITIATION LINE
-
+        m_tickspersecond.put(0, 0);
+        m_tickspersecond.put(1, 1000);
+        m_tickspersecond.put(2, 2000);
+        m_tickspersecond.put(3, 3000);
+        m_tickspersecond.put(4, 4000);
+        m_tickspersecond.put(5, 5000);
+        m_tickspersecond.put(6, 6000);
+        m_tickspersecond.put(7, 7000);
+        m_tickspersecond.put(8, 8000);
+        m_tickspersecond.put(9, 9000);
+        m_tickspersecond.put(10, 10000); 
+        m_tickspersecond.put(11, 11000);
+        m_tickspersecond.put(12, 12000);
+        m_tickspersecond.put(13, 13000);
     }
 
     public double getFlywheelTicksPerSecond(double distance) {
