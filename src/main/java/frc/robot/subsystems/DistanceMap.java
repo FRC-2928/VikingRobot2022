7428770c13a3642ac3465f763abb26b58469f8ec
BenFryHolman <himbafry@gmail.com>
2022-03-13 12:31:08 -0700

    DistanceMap changes

--- src/main/java/frc/robot/subsystems/DistanceMap.java
+++ src/main/java/frc/robot/subsystems/DistanceMap.java
@@ -22,20 +22,20 @@ public class DistanceMap {
     private final Map<Integer, Integer> m_tickspersecond = new HashMap<>();
 
     public void loadMaps() {
-        m_tickspersecond.put(0, 10000);
-        m_tickspersecond.put(1, 10000);
-        m_tickspersecond.put(2, 10000);
-        m_tickspersecond.put(3, 10000);
-        m_tickspersecond.put(4, 10000);
+        m_tickspersecond.put(0, 8500);
+        m_tickspersecond.put(1, 8500);
+        m_tickspersecond.put(2, 8500);
+        m_tickspersecond.put(3, 8500);
+        m_tickspersecond.put(4, 8500);
         m_tickspersecond.put(5, 10000);
-        m_tickspersecond.put(6, 90000);
-        m_tickspersecond.put(7, 10000);
-        m_tickspersecond.put(8, 10000);
-        m_tickspersecond.put(9, 10000);
-        m_tickspersecond.put(10, 10000); 
-        m_tickspersecond.put(11, 10000);
-        m_tickspersecond.put(12, 10000);
-        m_tickspersecond.put(13, 10000);
+        m_tickspersecond.put(6, 10000);
+        m_tickspersecond.put(7, 12000);
+        m_tickspersecond.put(8, 12000);
+        m_tickspersecond.put(9, 12000);
+        m_tickspersecond.put(10, 15000); 
+        m_tickspersecond.put(11, 15000);
+        m_tickspersecond.put(12, 15000);
+        m_tickspersecond.put(13, 15000);
     }
 
     public int getFlywheelTicksPerSecond(int distance) {
