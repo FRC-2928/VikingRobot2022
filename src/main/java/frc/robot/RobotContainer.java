d0f57f64abbbb896704b7dcd95efeb58791a465f
BenFryHolman <himbafry@gmail.com>
2022-04-25 20:03:40 -0700

    small auto tweak

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -128,7 +128,7 @@ public class RobotContainer {
 
     m_driverOI.getShiftButton().whenPressed(new InstantCommand(m_transmission::toggle, m_transmission));
 
-    m_autoChooser.setDefaultOption("Do Nothing", new SequentialCommandGroup(new WaitCommand(1.0)));
+    m_autoChooser.setDefaultOption("Do Nothing", new SequentialCommandGroup(new WaitCommand(0.1)));
     m_autoChooser.addOption("1-Ball Auto", new SequentialCommandGroup(new WaitCommand(1.0),
                                                                       new ShootOnce(m_intake, m_flywheel, m_turret), 
                                                                       new RunRamseteTrajectory(m_drivetrain, loadTrajectory("1BallAuto"))
